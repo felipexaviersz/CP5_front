005ac77c6e81af9bba7ff6540f0355834e4301de
Caue Garaude <cauegezaledo@gmail.com>
2024-10-03 19:02:11 -0300

    Finalizando o home

--- script.js
+++ script.js
@@ -28,3 +28,53 @@ audio.addEventListener('pause', () => {
 audio.addEventListener('ended', () => {
     audio.classList.remove('playing');
 });
+
+const audio2 = document.getElementById('meuAudio2');
+
+
+audio2.addEventListener('play', () => {
+    audio2.classList.add('playing');
+});
+
+
+audio2.addEventListener('pause', () => {
+    audio2.classList.remove('playing');
+});
+
+audio2.addEventListener('ended', () => {
+    audio2.classList.remove('playing');
+});
+
+const audio3 = document.getElementById('meuAudio3');
+
+
+audio3.addEventListener('play', () => {
+    audio3.classList.add('playing');
+});
+
+
+audio3.addEventListener('pause', () => {
+    audio3.classList.remove('playing');
+});
+
+audio3.addEventListener('ended', () => {
+    audio3.classList.remove('playing');
+});
+
+const audio4 = document.getElementById('meuAudio4');
+
+
+audio4.addEventListener('play', () => {
+    audio4.classList.add('playing');
+});
+
+
+audio4.addEventListener('pause', () => {
+    audio4.classList.remove('playing');
+});
+
+audio4.addEventListener('ended', () => {
+    audio4.classList.remove('playing');
+});
+
+
